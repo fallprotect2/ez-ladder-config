375f1b816d68a089349250305859d1324e193847
fallprotect2 <john.noriega@fallprotect.com>
2025-09-22 22:37:29 -0400

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import EzLadderConfigurator from './ez_ladder_configurator_refactored.jsx'
+import EzLadderConfigurator from './ez_ladder_configurator_refactored'
 
 createRoot(document.getElementById('root')!).render(<EzLadderConfigurator />)
